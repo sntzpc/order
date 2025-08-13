3740a4710ca7b0433a3895b6d192399ba9b71b0f
sntzpc <seriangtraining@gmail.com>
2025-08-13 08:09:27 +0700

    sniper

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // ==== CONFIG ====
 // Ganti URL berikut dengan URL "Web App" dari Apps Script setelah dideploy.
 const CONFIG = {
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbway2H870Tdh_ZepNx6QKhmDpWGhKvrTjgSaBTU2I3D1XQ9kZAufV7wx5p2G_DQ6vwI/exec'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxp7bjtToMOSXZKzU7RvXPQYpZW_a1WQPbPEdaQG0q_TnqjyUEHChBWNiwF8XOezBDP/exec'
 };
 
